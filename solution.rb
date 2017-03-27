356d7347778eaa82001d3722866f18282107475a
Rodman Aguirre Méndez <rodfagui@gmail.com>
2017-03-26 20:01:33 -0500

    Version 2

--- solution.rb
+++ solution.rb
@@ -1,9 +1,14 @@
 require 'sinatra'
 
-get '/' do
-	unless params[:nombre]
-		"Hola desconocido!"
-	else
-		"Hola #{params[:nombre]}!"
-	end
-end 
+# get '/' do
+# 	unless params[:nombre]
+# 		"Hola desconocido!"
+# 	else
+# 		"Hola #{params[:nombre]}!"
+# 	end
+# end 
+
+get '/maker/:nombre' do
+	@name = params[:nombre]
+	erb :index
+end
